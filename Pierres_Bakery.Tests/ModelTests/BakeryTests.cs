56e6f5e987aa5b975ba80ae537b893d8f60e30a0
Donovan Weber <donovanweber03@gmail.com>
2022-07-15 13:28:04 -0400

    Fix tests to work appropiately and use method to calc

--- Pierres_Bakery.Tests/ModelTests/BakeryTests.cs
+++ Pierres_Bakery.Tests/ModelTests/BakeryTests.cs
@@ -19,13 +19,21 @@ namespace PierresBakery.Tests
     {
       Bread newOrder = new Bread(1);
       int costOfOneBread = 5;
-      Assert.AreEqual(costOfOneBread,newOrder.CalculatePriceOfBread(1));
+      Assert.AreEqual(costOfOneBread, newOrder.CalculatePriceOfBread());
     }
+    [TestMethod]
     public void CalculatePriceOfBread_GetPriceOfTwoOrders_Int()
     {
       Bread newOrder = new Bread(2);
       int costOfTwoBread = 10;
-      Assert.AreEqual(costOfTwoBread, newOrder.CalculatePriceOfBread(2));
+      Assert.AreEqual(costOfTwoBread, newOrder.CalculatePriceOfBread());
+    }
+    [TestMethod]
+    public void CalculatePriceOfBread_GetPriceOfThreeWithDiscount_Int()
+    {
+      Bread newOrder = new Bread(3);
+      int costOfThreeBread = 10;
+      Assert.AreEqual(costOfThreeBread, 4);
     }
   }
 }
--- Pierres_Bakery/Models/Bakery.cs
+++ Pierres_Bakery/Models/Bakery.cs
@@ -1,16 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 namespace Bakery.Models
 {
   
   public class Bread
   {
+    private static int _Price = 5;
     public int AmountOfBread { get; set; }
-    
+    public int Price { get; set; }
     public Bread(int bread)
     {
       AmountOfBread = bread;
+      // Price = price; 
     }
+    public int CalculatePriceOfBread()
+    {
+      
+      int totalPrice = 0;
+      if (AmountOfBread == 0)
+      {
+        totalPrice += 0;
+      }
+      else if (AmountOfBread < 3)
+      {
+        for(int i = 0; i < AmountOfBread; i++)
+        {
+        totalPrice += _Price;
+        }
+      }
+      return totalPrice;
+    }
+
   }
   
   public class Pastry 
